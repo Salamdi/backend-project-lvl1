b9536d1f758537e634cc701f53cd723ff053404b
salamdi <dinislam91@gmail.com>
2021-03-21 20:05:04 +0300

    refactor game engine

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import readlineSync from 'readline-sync';
 import greeter from './cli.js';
 
-const MAX_COUNT = 3;
+const LAST_ROUND = 3;
 
 export default (game) => {
   const name = greeter();
   console.log(game.intro);
-  let count = MAX_COUNT;
+  let round = 0;
   do {
+    round += 1;
     const { question, answer } = game();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === answer) {
-      console.log('Correct!');
-    } else {
+
+    if (userAnswer !== answer) {
       console.log(
         `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`,
       );
       break;
     }
-    count -= 1;
-  } while (count);
-  if (count) {
+
+    console.log('Correct!');
+  } while (round < LAST_ROUND);
+  if (round < LAST_ROUND) {
     console.log(`Let's try again, ${name}!`);
   } else {
     console.log(`Congratulations, ${name}!`);
