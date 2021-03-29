7d9fa6b55c9d03e9ea3bb06cb5564cc17d5b6df5
salamdi <dinislam91@gmail.com>
2021-03-29 11:20:01 +0300

    refactor src/games/is-even-game.js

--- src/games/is-even-game.js
+++ src/games/is-even-game.js
@@ -3,11 +3,11 @@ import randomNumber from '../randomNumber.js';
 
 const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (n) => (n % 2 ? 'no' : 'yes');
+const isEven = (n) => (n % 2) == 0;
 
 const isEvenGame = () => {
   const question = randomNumber();
-  const answer = isEven(question);
+  const answer = isEven(question) ? 'yes' : 'no';
 
   return [question, answer];
 };
