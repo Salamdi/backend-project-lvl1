dabe33aacb9f30b2feacd0cd43f936c72364a8cc
salamdi <dinislam91@gmail.com>
2021-03-29 11:23:00 +0300

    use strict equality

--- src/games/is-even-game.js
+++ src/games/is-even-game.js
@@ -3,7 +3,7 @@ import randomNumber from '../randomNumber.js';
 
 const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (n) => (n % 2) == 0;
+const isEven = (n) => (n % 2) === 0;
 
 const isEvenGame = () => {
   const question = randomNumber();
